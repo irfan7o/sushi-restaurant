6b99d0a7ca28d01ec4248ab0c277954bd0512d72
kartolo727 <kartolo727@gmail.com>
2024-06-16 08:21:40 +0000

    Sushi Restaurant

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
-// Add JS here
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:1405499746.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:1906828878.
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
+
+if(navToggle) {
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:1348469711.
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose) {
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:1348469711.
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
